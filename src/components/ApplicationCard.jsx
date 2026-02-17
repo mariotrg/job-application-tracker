a1480631ec39e6383971c6e2c003c082ab2f03ec
Mario <193530526+mariotrg@users.noreply.github.com>
2026-02-17 14:12:12 +0000

    added button with viewApplication function

--- src/components/ApplicationCard.jsx
+++ src/components/ApplicationCard.jsx
@@ -1,13 +1,15 @@
-const ApplicationCard = (application) => {
+const ApplicationCard = ({ application, viewApplication }) => {
   return (
-    <div className="application_card" key={application.id}>
+    <div className="application_card">
       <h3>{application.position}</h3>
       <h4>{application.company}</h4>
       <p>{application.url}</p>
       <p>{application.applicationStatus}</p>
       <p>{application.applicationDate}</p>
 
-      <button>View Details</button>
+      <button onClick={() => viewApplication(application.id)}>
+        View Details
+      </button>
     </div>
   );
 };
