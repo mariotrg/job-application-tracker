8cef8328e5158428c33176bab17188b59cbeb77e
Mario <193530526+mariotrg@users.noreply.github.com>
2026-02-17 12:55:30 +0000

    used useEffect to fetch data from db.json, return the application list

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./index.css";
+
+import ApplicationsList from "./components/ApplicationsList";
+
 const App = () => {
-  return <h1>Job Application Tracker</h1>;
+  const [applications, setApplications] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/applications").then((response) => {
+      setApplications(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <ApplicationsList applications={applications} />
+    </div>
+  );
 };
 
 export default App;
