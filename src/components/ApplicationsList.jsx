d6a87f08eb79fbbd943f6a8666e059d2da72d7cc
Mario <193530526+mariotrg@users.noreply.github.com>
2026-02-17 14:14:34 +0000

    refactor applicationList: fixed the key, added viewApplication prop

--- src/components/ApplicationsList.jsx
+++ src/components/ApplicationsList.jsx
@@ -1,10 +1,16 @@
 import ApplicationCard from "./ApplicationCard";
 
-const ApplicationsList = ({ applications }) => {
+const ApplicationsList = ({ applications, viewApplication }) => {
   return (
     <div className="application_list">
       {applications.map((application) => {
-        return ApplicationCard(application);
+        return (
+          <ApplicationCard
+            key={application.id}
+            application={application}
+            viewApplication={viewApplication}
+          />
+        );
       })}
     </div>
   );
