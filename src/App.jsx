18457335f4b1eb9fbb36e4060db4082babd0edb4
Mario <193530526+mariotrg@users.noreply.github.com>
2026-02-20 14:23:26 +0000

    added all created components. two new states. functions to handle view changes, form changes and add new application. misc fixes

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,27 @@ import axios from "axios";
 import Applications from "./services/applications";
 import ApplicationsList from "./components/ApplicationsList";
 import Detail from "./components/Detail";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import Button from "./components/Button";
+import Form from "./components/Form";
 
 const App = () => {
   const [applications, setApplications] = useState([]);
-  const [view, setView] = useState("list");
+  const [view, setView] = useState("home");
   const [currentApplication, setCurrentApplication] = useState(null);
   const [newStatus, setNewStatus] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [newApplication, setNewApplication] = useState({
+    position: "",
+    company: "",
+    url: "",
+    applicationDate: "",
+    applicationStatus: "",
+    notes: "",
+    source: "",
+  });
 
   const statusList = ["Applied", "Interview", "Offer", "Rejected", "Withdrawn"];
 
@@ -38,6 +53,40 @@ const App = () => {
     setNewStatus(e.target.value);
   };
 
+  const handleViewClick = (view) => {
+    setView(view);
+  };
+
+  const handleFormView = () => {
+    setIsOpen(true);
+  };
+
+  const handleFormClose = () => {
+    setIsOpen(false);
+  };
+
+  const handleFormChange = (e) => {
+    setNewApplication({ ...newApplication, [e.target.name]: e.target.value });
+  };
+
+  const addNew = (e) => {
+    e.preventDefault();
+
+    Applications.addNew(newApplication).then((returnedApplication) => {
+      setApplications(applications.concat(returnedApplication));
+    });
+
+    setNewApplication({
+      position: "",
+      company: "",
+      url: "",
+      applicationDate: "",
+      applicationStatus: "",
+      notes: "",
+      source: "",
+    });
+  };
+
   const deleteApplication = (id) => {
     if (window.confirm("Do you want to delete this application?")) {
       Applications.deleteApplication(id)
@@ -48,7 +97,7 @@ const App = () => {
           handleBack();
         })
         .catch((error) => {
-          alert("The application has already been deleted");
+          alert("The application has already been deleted.");
           setApplications(
             applications.filter((application) => application.id !== id),
           );
@@ -61,38 +110,76 @@ const App = () => {
     const application = applications.find((n) => n.id === id);
     const changedApplication = { ...application, applicationStatus: newStatus };
 
-    Applications.updateApplication(id, changedApplication).then(
-      (updatedApplication) => {
+    Applications.updateApplication(id, changedApplication)
+      .then((updatedApplication) => {
         setApplications(
           applications.map((application) =>
             application.id === id ? updatedApplication : application,
           ),
         );
-      },
-    );
+      })
+      .catch((error) => {
+        alert("The application does not exist anymore.");
+        setApplications(
+          applications.filter((application) => application.id !== id),
+        );
+        handleBack();
+      });
+    setNewStatus(null);
   };
 
   return (
-    <div>
-      {view === "list" && (
-        <ApplicationsList
-          applications={applications}
-          viewApplication={viewApplication}
-        />
-      )}
+    <>
+      <Header />
+      <main>
+        {view === "home" && (
+          <>
+            <Dashboard />
+            <Button
+              onClick={handleFormView}
+              text={"+"}
+              style="add_new_button"
+            />
+          </>
+        )}
 
-      {view === "application" && (
-        <Detail
-          application={selectedApplication}
-          handleBack={handleBack}
-          statusList={statusList}
-          deleteApplication={deleteApplication}
-          updateApplication={updateApplication}
-          handleSelectChange={handleSelectChange}
-          newStatus={newStatus}
-        />
-      )}
-    </div>
+        {view === "list" && (
+          <>
+            <ApplicationsList
+              applications={applications}
+              viewApplication={viewApplication}
+            />
+            <Button
+              onClick={handleFormView}
+              text={"+"}
+              style="add_new_button"
+            />
+          </>
+        )}
+
+        {view === "application" && (
+          <Detail
+            application={selectedApplication}
+            handleBack={handleBack}
+            statusList={statusList}
+            deleteApplication={deleteApplication}
+            updateApplication={updateApplication}
+            handleSelectChange={handleSelectChange}
+            newStatus={newStatus}
+          />
+        )}
+      </main>
+
+      <Navigation handleViewClick={handleViewClick} />
+      <Form
+        isOpen={isOpen}
+        onClick={handleFormClose}
+        statusList={statusList}
+        onChange={handleFormChange}
+        onSubmit={addNew}
+        newApplication={newApplication}
+      />
+    </>
   );
 };
 
