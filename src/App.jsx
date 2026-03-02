a9b34cec6ce66456a76e0e6efd8d09d2e131c4f1
Mario <193530526+mariotrg@users.noreply.github.com>
2026-03-02 19:02:03 +0000

    feat: setup json-server and fetch job applications using axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const App = () => {
+  const [applications, setApplications] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/applications").then((response) => {
+      setApplications(response.data);
+    });
+  }, []);
+
   return (
     <div>
-      <h1>Hello World!</h1>
+      {applications.map((item) => (
+        <p key={item.id}>{item.position}</p>
+      ))}
     </div>
   );
 };
